aea0fb0ad0776d71f000dc475ad0f5ed9b6cd3c5
JeanMichel FRANCOIS <toutpt@gmail.com>
2012-01-25 14:49:57 +0100

    push resources and first integration

--- plone/app/jqueryui/interfaces.py
+++ plone/app/jqueryui/interfaces.py
@@ -1,4 +1,102 @@
-from zope import interface
+from zope.interface import Interface
+from zope import schema
 
-class ILayer(interface.Interface):
-    """Browser Layer"""
+class IJqueryUILayer(Interface):
+    """Browser layer"""
+
+class IJQueryUIPlugins(Interface):
+    
+    ui_core = schema.Bool(title=u"Core",
+                       description=u"The core of jQuery UI, required for all interactions and widgets.",
+                       required=False,default=False)
+    
+    ui_widget = schema.Bool(title=u"Widget",
+                         description=u"",required=False,default=False)
+
+    ui_mouse = schema.Bool(title=u"Mouse",
+                         description=u"",required=False,default=False)
+
+    ui_position = schema.Bool(title=u"Position",
+                         description=u"",required=False,default=False)
+
+    ui_draggable = schema.Bool(title=u"Draggable",
+                         description=u"",required=False,default=False)
+
+    ui_droppable = schema.Bool(title=u"Droppable",
+                         description=u"",required=False,default=False)
+
+    ui_resizable = schema.Bool(title=u"Resizable",
+                         description=u"",required=False,default=False)
+
+    ui_selectable = schema.Bool(title=u"Selectable",
+                         description=u"",required=False,default=False)
+
+    ui_sortable = schema.Bool(title=u"Sortable",
+                         description=u"",required=False,default=False)
+
+    ui_accordion = schema.Bool(title=u"Accordion",
+                         description=u"",required=False,default=False)
+
+    ui_autocomplete = schema.Bool(title=u"Autocomplete",
+                         description=u"",required=False,default=False)
+
+    ui_button = schema.Bool(title=u"Button",
+                         description=u"",required=False,default=False)
+
+    ui_dialog = schema.Bool(title=u"Dialog",
+                         description=u"",required=False,default=False)
+
+    ui_slider = schema.Bool(title=u"Slider",
+                         description=u"",required=False,default=False)
+
+    ui_tabs = schema.Bool(title=u"Tabs",
+                         description=u"",required=False,default=False)
+
+    ui_datepicker = schema.Bool(title=u"Date picker",
+                         description=u"",required=False,default=False)
+
+    ui_progressbar = schema.Bool(title=u"Progress bar",
+                         description=u"",required=False,default=False)
+
+    effects_core = schema.Bool(title=u"Effects 'core'",
+                         description=u"",required=False,default=False)
+
+    effects_blind = schema.Bool(title=u"Effects 'blind'",
+                         description=u"",required=False,default=False)
+
+    effects_bounce = schema.Bool(title=u"Effects 'bounce'",
+                         description=u"",required=False,default=False)
+
+    effects_clip = schema.Bool(title=u"Effects 'clip'",
+                         description=u"",required=False,default=False)
+
+    effects_drop = schema.Bool(title=u"Effects 'drop'",
+                         description=u"",required=False,default=False)
+
+    effects_explode = schema.Bool(title=u"Effects 'explode'",
+                         description=u"",required=False,default=False)
+
+    effects_fade = schema.Bool(title=u"Effects 'fade'",
+                         description=u"",required=False,default=False)
+
+    effects_fold = schema.Bool(title=u"Effects 'fold",
+                         description=u"",required=False,default=False)
+
+    effects_highlight = schema.Bool(title=u"Effects 'highlight'",
+                         description=u"",required=False,default=False)
+
+    effects_pulsate = schema.Bool(title=u"Effects 'pulsate'",
+                         description=u"",required=False,default=False)
+
+    effects_scale = schema.Bool(title=u"Effects 'scale'",
+                         description=u"",required=False,default=False)
+
+    effects_shake = schema.Bool(title=u"Effects 'shake'",
+                         description=u"",required=False,default=False)
+
+    effects_slide = schema.Bool(title=u"Effects 'slide'",
+                         description=u"",required=False,default=False)
+
+    effects_transfer = schema.Bool(title=u"Effects 'transfer'",
+                         description=u"",required=False,default=False)
+    
---
+++ plone/app/jqueryui/resources.py
@@ -0,0 +1,92 @@
+import logging
+from copy import copy
+from zope import component
+from plone.registry.interfaces import IRegistry, IRecordModifiedEvent
+from Products.Five import BrowserView
+from StringIO import StringIO
+
+from plone.app.jqueryui import interfaces, config
+
+logger = logging.getLogger('plone.app.jqueryui')
+
+class PloneAppJQueryUICustomJS(BrowserView):
+    """This view aggregate javascripts according to the configuration. It has
+    been created to not polute the portal_javascripts will all plugins"""
+    
+    def __call__(self):
+        settings = self.settings
+
+        if settings is None:
+            return
+        resources = self.get_resources()
+
+        if not resources:
+            return
+
+        return self.get_resources_content(resources)
+
+    def settings(self):
+        """Return records from portal_registry corresponding to IJQueryUIPlugins
+        """
+        registry = component.queryUtility(IRegistry)
+
+        if registry is None:
+            return
+
+        return registry.forInterface(interfaces.IJQueryUIPlugins, None)
+
+    def get_resources(self):
+        """Return a list of resources (at least their ids) computed from
+        the configuration provided by IJQueryUIPlugins's records in plone
+        portal_registry"""
+        settings = self.settings()
+        deps = config.JQUERYUI_DEPENDENCIES
+        all = deps.keys()
+        resources = []
+        wanted = [] #not ordered list of wanted plugins
+        tpl = "++resource++plone.app.jqueryui/js/jquery.%s.min.js"
+        ordered_plugins = copy(config.ORDERED_PLUGINS)
+
+        for plugin in all:
+            attr_name = plugin.replace('.','_')
+            is_wanted = getattr(settings, attr_name, False)
+
+            if is_wanted:
+                wanted.append(plugin)
+                deps = config.JQUERYUI_DEPENDENCIES[plugin]
+                for dep in deps:
+                    wanted.append(dep)
+
+        for plugin in all:
+            if plugin not in wanted:
+                ordered_plugins.remove(plugin)
+
+        for plugin in ordered_plugins:
+            resources.append(tpl%plugin)
+
+        return resources
+
+
+    def get_resources_content(self, resources):
+        """Resources must be a list of resource ids.
+        This method return the content of each resources appended in one string
+        """
+        data = StringIO()
+
+        if resources is None:
+            resources = self.get_resources()
+
+
+        for resourceid in resources:
+            try:
+                resource = self.context.restrictedTraverse(resourceid)
+            except KeyError,e:
+                logger.error(e)
+                continue
+
+            if not resource:
+                continue
+
+            data.write(open(resource.context.path).read())
+ 
+        return data.getvalue()
